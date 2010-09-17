6a1694ddf4db5b9c846c87f79c7a64dac26016e9
Robert Palmer <robert.palmer@artcom.de>
2010-09-17 11:58:11 +0200

    added Header for the Framework

---
+++ Classes/HoccerClient/Hoccer.h
@@ -0,0 +1,10 @@
+//
+//  Hoccer.h
+//  HoccerAPI
+//
+//  Created by Robert Palmer on 17.09.10.
+//  Copyright 2010 Hoccer GmbH. All rights reserved.
+//
+
+#import "HCClient.h"
+#import "HCGeoStorage.h"
