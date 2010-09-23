46bee95787f364c2140d025400a4a476b2739d1b
Robert Palmer <robert.palmer@artcom.de>
2010-09-23 17:07:46 +0200

    pointing to geostore beta server

--- Sample/Classes/GeostoreSampleViewController.m
+++ Sample/Classes/GeostoreSampleViewController.m
@@ -80,19 +80,6 @@
 }
 
 
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
-
 - (void)dealloc {
 	[geostorage release];
 	[mapView release];
